18971dbd4ecdbcab85a53852175c1c0651959067
Lumi Ciobanu <lumiciobanu@gmail.com>
2020-07-26 23:09:02 +0300

    sunday night commit - last commit

--- sprint-two/scripts/script.js
+++ sprint-two/scripts/script.js
@@ -1,4 +1,6 @@
 
+// Note to TA: There are a few errors I didn't know how to fix. I need more help to understand how to finish this 2 requierments
+
 /* Biography Comment */
 
 let commentList = [
@@ -36,58 +38,58 @@ form.addEventListener('submit', function(event) {
 
 
 /* Shows Table Tickets */
-// let showList = [
-//     {
-//         dates: "Mon Dec 17 2018",
-//         venue: "Ronald Lane",
-//         location: "San Francisco, CA",
-//         button: '.button'
-//     },
+let showList = [
+    {
+        dates: "Mon Dec 17 2018",
+        venue: "Ronald Lane",
+        location: "San Francisco, CA",
+        button: '.button'
+    },
 
-//     {
-//         dates: "Tue Jul 18 2019",
-//         venue: "Pier 3 East",
-//         location: "San Francisco, CA",
-//         button: '.button'
-//     },
+    {
+        dates: "Tue Jul 18 2019",
+        venue: "Pier 3 East",
+        location: "San Francisco, CA",
+        button: '.button'
+    },
 
-// ]
+]
 
-// let ticketTable = document.querySelector('.shows__table');
-
-
-// for (var i = 0; i < showList.length; i++) {
-//     let rowLabel = document.createElement('div');
-//     rowLabel.classList.add('.show__row--label');
+let ticketTable = document.querySelector('.shows__table');
 
 
+for (var i = 0; i < showList.length; i++) {
+    let rowLabel = document.createElement('div');
+    rowLabel.classList.add('.show__row--label');
 
-//     let itemLabel = document.createElement('p');
-//     itemLabel.classList.add('.shows__label');
-//     itemLabel.innerText = "Dates";
-//     itemLabel.innerText = "Venue";
-//     itemLabel.innerText = "Location";
 
-//    let itemLabel = document.createElement('p');
-//     itemLabel.classList.add('.shows__label--hidden');
-//     itemLabel.innerText = "";
 
-//     let row = document.createElement('div');
-//     row.classList.add('.show__row');
+    let itemLabel = document.createElement('p');
+    itemLabel.classList.add('.shows__label');
+    itemLabel.innerText = "Dates";
+    itemLabel.innerText = "Venue";
+    itemLabel.innerText = "Location";
+
+   let itemLabel = document.createElement('p');
+    itemLabel.classList.add('.shows__label--hidden');
+    itemLabel.innerText = "";
+
+    let row = document.createElement('div');
+    row.classList.add('.show__row');
     
 
-//     let showInfo = document.createElement('p');
-//     showInfo.classList.add('.shows__info');
-//     info.innerText = showList[i].dates;
+    let showInfo = document.createElement('p');
+    showInfo.classList.add('.shows__info');
+    info.innerText = showList[i].dates;
     
    
-//     ticketTable.appendChild(rowLabel);
-//     rowLabel.appendChild(itemLabel);
-//     ticketTable.appendChild(row);
-//     rowLabel.appendChild(showInfo);
+    ticketTable.appendChild(rowLabel);
+    rowLabel.appendChild(itemLabel);
+    ticketTable.appendChild(row);
+    rowLabel.appendChild(showInfo);
    
 
-// }
+}
 
 
 
