88f36149ad0721cef5726aabe831e855afd8a764
Lumi Ciobanu <lumiciobanu@gmail.com>
2020-08-01 12:11:10 +0300

    saturday morning commit - before feedback

--- sprint-three/scripts/comments.js
+++ sprint-three/scripts/comments.js
@@ -1,26 +1,28 @@
 
-let commentList = [
-    {
-        name: "Micheal Lyons",
-        date: "12/04/2019",
-        comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-        image: "../assets/images/Mohan-muruge.jpg"
-    },
+let API_key = "5fe5aa5f-8066-497d-93b9-6fa22bdf1301"
 
-    {
-        name: "Gary Wong",
-        date: "12/04/2019",
-        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-        image: "../assets/images/male-placeholder-image.jpeg"
-    },
+// let commentList = [
+//     {
+//         name: "Micheal Lyons",
+//         date: "12/04/2019",
+//         comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+//         image: "../assets/images/Mohan-muruge.jpg"
+//     },
 
-    {
-        name: "Theodore Duncan",
-        date: "12/04/2019",
-        comment: "How can someone be so good. You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-        image: "../assets/images/male-placeholder-image.jpeg"
-    }
-]
+//     {
+//         name: "Gary Wong",
+//         date: "12/04/2019",
+//         comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+//         image: "../assets/images/male-placeholder-image.jpeg"
+//     },
+
+//     {
+//         name: "Theodore Duncan",
+//         date: "12/04/2019",
+//         comment: "How can someone be so good. You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+//         image: "../assets/images/male-placeholder-image.jpeg"
+//     }
+// ]
 
 const form = document.getElementById('form');
 // building the NEW comments section using DOM
@@ -35,7 +37,7 @@ function displayComment(commentObject) {
 
     let userImg = document.createElement('img')
     userImg.classList.add('comments__image')
-    userImg.src = commentList[i].image;
+    userImg.src = "../assets/images/Mohan-muruge.jpg";
 
     let addComment = document.createElement('div')
     addComment.classList.add('comments__add')
@@ -49,7 +51,9 @@ function displayComment(commentObject) {
 
     let commentsDate = document.createElement('p')
     commentsDate.classList.add('comments__date')
-    commentsDate.innerText = commentObject.date
+    let commentDay = new Date(commentObject.timestamp)
+    commentsDate.innerText = DateObjectFormatting(commentDay)
+
 
     let commentsText = document.createElement('div')
     commentsText.classList.add('comments__text')
@@ -72,11 +76,20 @@ function displayComment(commentObject) {
     console.log(newComment)
 }
 
-// creating a for loop for the commentList built with these elements
-for (i = 0; i < commentList.length; i++) {
-    displayComment(commentList[i])
-
+// function will accept a date and returns the date in month/day/year
+function DateObjectFormatting(date) {
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    return month + "/" + day + "/" + year;
 }
+
+
+// creating a for loop for the commentList built with these elements
+// for (i = 0; i < commentList.length; i++) {
+//     displayComment(commentList[i])
+
+// }
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -84,21 +97,72 @@ form.addEventListener('submit', function (event) {
     let userComment = event.target.subject.value;
     let newComment = {
         name: userInput,
-        comment: userComment,
-        date: "07/29/2020"
-    }
-
-    commentList.unshift(newComment); //adding new comment to the beginning of the array & reseting
-    form.reset();
+        comment: userComment
+         }
+    
+   
     commentBox.innerHTML = '';
+   
+    getComments();
+    
 
+    // commentList.unshift(newComment); //adding new comment to the beginning of the array & reseting
+    
+    
+    form.reset();
 
-    //displaying the comments after adding the new one
-    for (i = 0; i < commentList.length; i++) {
-        displayComment(commentList[i])
-    }
+    // displaying the comments after adding the new one
+    // for (i = 0; i < commentList.length; i++) {
+    //     displayComment(commentList[i])
+    // }
 
 });
 
+
+
+//
+
+getComments = () => {
+    axios.get("https://project-1-api.herokuapp.com/comments?api_key=5fe5aa5f-8066-497d-93b9-6fa22bdf1301")
+        .then(results => {
+            let comments = results.data
+            console.log(comments)
+
+            for (i = 0; i < comments.length; i++) {
+                displayComment(comments[i])
+            }
+
+            // postComments(results)
+        })
+
+        .catch(err => console.log(err))
+};
+
+getComments();
+
+getComments() = (results, i) => {
+    let newComment = document.createElement('div')
+    newComment.classList.add('comments__new')
+
+    commentBox.appendChild(newComment)
+}
+
+
+
+postComment = (newComment) => {
+
+    axios.post("https://project-1-api.herokuapp.com/comments?api_key=5fe5aa5f-8066-497d-93b9-6fa22bdf1301", {
+            name: newComment.name,
+           comment: newComment.comment
+        })
+    
+        .then(function(response) {
+            console.log(response);
+        })
+         .catch (err => console.log(err));
+        
+};
+
+postComment(newComment);
 
 
--- sprint-three/scripts/script.js
+++ sprint-three/scripts/script.js
@@ -1,24 +1,23 @@
 
-// // Note to TA: There are a few errors I didn't know how to fix. I need more help to understand how to finish this 2 requierments
 
 
 /* Shows Table Tickets */
-let showList = [
-    {
-        dates: "Mon Dec 17 2018",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA",
+// let showList = [
+//     {
+//         dates: "Mon Dec 17 2018",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA",
         
-    },
+//     },
 
-    {
-        dates: "Tue Jul 18 2019",
-        venue: "Pier 3 East",
-        location: "San Francisco, CA",
+//     {
+//         dates: "Tue Jul 18 2019",
+//         venue: "Pier 3 East",
+//         location: "San Francisco, CA",
         
-    },
+//     },
 
-]
+// ]
 
 
 let ticketTable = document.querySelector('.shows__table');
@@ -54,7 +53,7 @@ rowLabel.appendChild(itemLabelLocation)
 rowLabel.appendChild(itemLabelButton)
 
 
-
+function showCreate(showList) { 
  for (var i = 0; i < showList.length; i++) {
    /* Second Row */
 let Row = document.createElement('div');
@@ -68,7 +67,7 @@ itemLabelRow.innerText = "Dates";
 let showInfo = document.createElement('p');
 showInfo.classList.add('show__info')
 showInfo.classList.add('show__info--date')
-showInfo.innerText = showList[i].dates ;
+showInfo.innerText = showList[i].date ;
 
 let itemLabelRowVenue = document.createElement('p');
 itemLabelRowVenue.classList.add('shows__label');
@@ -77,7 +76,7 @@ itemLabelRowVenue.innerText = "Venue";
 
 let showInfoVenue = document.createElement('p');
 showInfoVenue.classList.add('show__info')
-showInfoVenue.innerText = showList[i].venue ;
+showInfoVenue.innerText = showList[i].place ;
 
 let itemLabelRowLocation = document.createElement('p');
 itemLabelRowLocation.classList.add('shows__label');
@@ -107,8 +106,24 @@ Row.appendChild(showInfoButton);
 showInfoButton.appendChild(ticketButton);
 
  }
+}
 
 
+ getShows = () => {
+    axios.get("https://project-1-api.herokuapp.com/showdates?api_key=5fe5aa5f-8066-497d-93b9-6fa22bdf1301")
+        .then(results => {
+            let shows = results.data
+            console.log(shows)
+      
+           showCreate(shows)              
+
+                    
+        })
+        
+        .catch(err => console.log(err))
+};
+
+getShows();
 
 
 
